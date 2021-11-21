83ca8fb6a04e2087fdcc6e9013fcd04332acada6
yuricxd350 <yuricarvfeitosa.yc@gmail.com>
2021-11-21 20:40:39 -0300

    Crud-Agendamento

--- src/main/java/med/saude/web/controller/AgendamentoController.java
+++ src/main/java/med/saude/web/controller/AgendamentoController.java
@@ -1,20 +1,67 @@
 package med.saude.web.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import med.saude.domain.Agendamento;
+import med.saude.domain.Especialidade;
+import med.saude.domain.Medico;
+import med.saude.domain.Paciente;
+import med.saude.service.AgendamentoService;
+import med.saude.service.EspecialidadeService;
+import med.saude.service.MedicoService;
+import med.saude.service.PacienteService;
 
 @Controller
 @RequestMapping("/agendamentos")
 public class AgendamentoController {
+	
+	@Autowired
+	private AgendamentoService agendamentoService;
+
 
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Agendamento agendamento) {
 		return "/agendamento/cadastro";
 	}
 	
 	@GetMapping("/listar")
-	public String listar() {
+	public String listar(ModelMap model) {
+		model.addAttribute("agendamentos", agendamentoService.buscarTodos());
 		return "/agendamento/lista";
+	}
+	
+	@PostMapping("/salvar")
+	public String salvar(Agendamento agendamento) {
+		agendamentoService.salvar(agendamento);
+		return "redirect:/agendamentos/cadastrar";
+	}
+	
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+		model.addAttribute("agendamento", agendamentoService.buscarPorId(id));
+		return "/agendamento/cadastro";
+	}
+	
+	@PostMapping("/editar")
+	public String editar(Agendamento agendamento) {
+		agendamentoService.editar(agendamento);
+		return "redirect:/agendamentos/listar";
+	}
+	
+	@GetMapping("/excluir/{id}")
+	public String excluir(@PathVariable("id") Long id, ModelMap model) {
+		agendamentoService.excluir(id);
+		return listar(model);
+		 
 	}
 }
--- src/main/java/med/saude/web/controller/MedicoController.java
+++ src/main/java/med/saude/web/controller/MedicoController.java
@@ -20,28 +20,28 @@ import med.saude.service.MedicoService;
 @Controller
 @RequestMapping("/medicos")
 public class MedicoController {
-	
-	@Autowired
-	private MedicoService medicoService;
-	@Autowired
-	private EspecialidadeService especialidadeService;
+    
+    @Autowired
+    private MedicoService medicoService;
+    @Autowired
+    private EspecialidadeService especialidadeService;
 
-	@GetMapping("/cadastrar")
-	public String cadastrar(ModelMap model) {
-		Medico medico = new Medico();
-		List <Especialidade> especialidade = especialidadeService.buscarTodos(); 
-		model.addAttribute("medico", medico);
-		model.addAttribute("especialidade", especialidade);
-		return "/medico/cadastro";
-	}
-	
-	@GetMapping("/listar")
-	public String listar(ModelMap model) {
-		model.addAttribute("medicos", medicoService.buscarTodos());
-		return "/medico/lista";
-	}
-	
-	@PostMapping("/salvar")
+    @GetMapping("/cadastrar")
+    public String cadastrar(ModelMap model) {
+        Medico medico = new Medico();
+        List <Especialidade> especialidade = especialidadeService.buscarTodos(); 
+        model.addAttribute("medico", medico);
+        model.addAttribute("especialidade", especialidade);
+        return "/medico/cadastro";
+    }
+    
+    @GetMapping("/listar")
+    public String listar(ModelMap model) {
+        model.addAttribute("medicos", medicoService.buscarTodos());
+        return "/medico/lista";
+    }
+    
+    @PostMapping("/salvar")
     public String salvar(@ModelAttribute Medico medico, BindingResult resultado) {
         Especialidade especialidade = new Especialidade();
         especialidade.setId(Long.valueOf(resultado.getFieldValue("especialidade").toString()));
@@ -49,23 +49,23 @@ public class MedicoController {
         medicoService.salvar(medico);
         return "redirect:/medicos/cadastrar";
     }
-	
-	@GetMapping("/editar/{id}")
-	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
-		model.addAttribute("medico", medicoService.buscarPorId(id));
-		return "/medico/cadastro";
-	}
-	
-	@PostMapping("/editar")
-	public String editar(Medico medico) {
-		medicoService.editar(medico);
-		return "redirect:/medicos/listar";
-	}
-	
-	@GetMapping("/excluir/{id}")
-	public String excluir(@PathVariable("id") Long id, ModelMap model) {
-		medicoService.excluir(id);
-		return listar(model);
-		 
-	}
+    
+    @GetMapping("/editar/{id}")
+    public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+        model.addAttribute("medico", medicoService.buscarPorId(id));
+        return "/medico/cadastro";
+    }
+    
+    @PostMapping("/editar")
+    public String editar(Medico medico) {
+        medicoService.editar(medico);
+        return "redirect:/medicos/listar";
+    }
+    
+    @GetMapping("/excluir/{id}")
+    public String excluir(@PathVariable("id") Long id, ModelMap model) {
+        medicoService.excluir(id);
+        return listar(model);
+         
+    }
 }
